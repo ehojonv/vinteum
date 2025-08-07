4f30750e624afc1d01b5c05a925f6c23cc20bcac
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-08-07 09:54:35 -0300

    Codigo aula 1

---
+++ src/main/java/com/example/Computador.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Computador extends Jogador {
+
+    @Override
+    public boolean parou() {
+        return getPontos() > 16 ? true : false;
+    }
+
+    
+    
+}
